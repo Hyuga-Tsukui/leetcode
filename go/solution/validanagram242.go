5e795f6eb501b2ba6c439a659d427a67f3597f87
Hyuga Tsukui <gakikiki407@gmail.com>
2024-03-16 12:01:42 +0900

    update

--- go/solution/validanagram242.go
+++ go/solution/validanagram242.go
@@ -1,20 +1,18 @@
 package solution
 
+import (
+	"maps"
+)
+
 func isAnagram(s string, t string) bool {
 	if len(s) != len(t) {
 		return false
 	}
-	m := map[rune]int{}
-	for _, r := range s {
-		m[r]++
+	a := map[byte]int{}
+	b := map[byte]int{}
+	for i := 0; i < len(s); i++ {
+		a[s[i]]++
+		b[t[i]]++
 	}
-	for _, r := range t {
-		if c, ok := m[r]; ok && c > 0 {
-			m[r]--
-			continue
-		} else {
-			return false
-		}
-	}
-	return true
+	return maps.Equal(a, b)
 }
