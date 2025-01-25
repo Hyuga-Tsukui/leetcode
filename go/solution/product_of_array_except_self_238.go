aeb1f89563c7526cdbee17e9997cfc7131679ced
Hyuga Tsukui <gakikiki407@gmail.com>
2025-01-25 12:02:15 +0900

    fix: solution eficency

--- go/solution/product_of_array_except_self_238.go
+++ go/solution/product_of_array_except_self_238.go
@@ -1,21 +1,48 @@
 package solution
 
 func productExceptSelf(nums []int) []int {
-	result := make([]int, len(nums))
+	n := len(nums)
+	result := make([]int, n)
+	prefix := make([]int, n)
+	suffix := make([]int, n)
 
-	// initialize.
-	for i := 0; i < len(result); i++ {
-		result[i] = 1
+	// 先頭のprefixはsuffixとの計算を考えると必ず1になる.
+	prefix[0] = 1
+	for i := 1; i < n; i++ {
+		prefix[i] = nums[i-1] * prefix[i-1]
 	}
 
-	for i := 0; i < len(nums); i++ {
-		for j := 0; j < len(result); j++ {
-			if j == i {
-				continue
-			}
-			result[j] = result[j] * nums[i]
-		}
+	// 末端のsuffixは1.
+	suffix[n-1] = 1
+
+	for i := n - 2; i >= 0; i-- {
+		suffix[i] = nums[i+1] * suffix[i+1]
+	}
+
+	for i := 0; i < n; i++ {
+		result[i] = suffix[i] * prefix[i]
 	}
 
 	return result
 }
+
+// naive solution
+// func productExceptSelf(nums []int) []int {
+// 	result := make([]int, len(nums))
+//
+// 	// initialize.
+// 	for i := 0; i < len(result); i++ {
+// 		result[i] = 1
+// 	}
+//
+// 	for i := 0; i < len(nums); i++ {
+// 		for j := 0; j < len(result); j++ {
+// 			if j == i {
+// 				continue
+// 			}
+// 			result[j] = result[j] * nums[i]
+// 		}
+// 	}
+//
+// 	return result
+// }
